4bd6741262540a21c7c70edfc88452bc967b6132
Saad Choudhry <saadchoudhry0@gmail.com>
2024-06-13 00:55:10 +0100

    Update indentation in script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
-  loadComponent("header.html", "#header");
-  loadComponent("footer.html", "#footer");
-  loadComponent("footer-mobile.html", "#footer-mobile");
+    loadComponent("header.html", "#header");
+    loadComponent("footer.html", "#footer");
+    loadComponent("footer-mobile.html", "#footer-mobile");
 
-  function loadComponent(url, selector) {
-      fetch(url)
-          .then(response => response.text())
-          .then(data => {
-              document.querySelector(selector).innerHTML = data;
-              if (selector === "#footer" || selector === "#footer-mobile") {
-                  setupNewsletterForm();
-              }
-          })
-          .catch(error => console.error('Error loading component:', error));
-  }
+    function loadComponent(url, selector) {
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                document.querySelector(selector).innerHTML = data;
+                if (selector === "#footer" || selector === "#footer-mobile") {
+                    setupNewsletterForm();
+                }
+            })
+            .catch(error => console.error('Error loading component:', error));
+    }
 
-  function setupNewsletterForm() {
-      const form = document.getElementById('newsletter-form');
-      const submissionMessage = document.getElementById('submission-message');
+    function setupNewsletterForm() {
+        const form = document.getElementById('newsletter-form');
+        const submissionMessage = document.getElementById('submission-message');
 
-      form.addEventListener('submit', function(event) {
-          event.preventDefault();
-          form.style.display = 'none';
-          submissionMessage.style.display = 'block';
-      });
-  }
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.style.display = 'none';
+            submissionMessage.style.display = 'block';
+        });
+    }
 
-  function setupNewsletterForm() {
-    const form = document.getElementById('contact-form');
-    const submissionMessage = document.getElementById('message-sent');
+    function setupNewsletterForm() {
+        const form = document.getElementById('contact-form');
+        const submissionMessage = document.getElementById('message-sent');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.style.display = 'none';
-        submissionMessage.style.display = 'block';
-    });
-}
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.style.display = 'none';
+            submissionMessage.style.display = 'block';
+        });
+    }
 });
